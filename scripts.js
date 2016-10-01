34f28b4a161ac595924dff8230edf0b0eb3a9fa0
rthakker549 <rthakker549@gmail.com>
2016-10-01 11:20:34 -0500

    Done IG

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP/HW2-Template/master/logan.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-rthakker549/master/rohan.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
-    $.each(data.nickNames, function(key, value){
-      $("#nicknames").append("<li>" + value.nick + "</li>");
+    $.each(data.topStrengths, function(key, value){
+      $("#topStrengths").append("<li>" + value.top2 + "</li>");
     });
   });
 });
